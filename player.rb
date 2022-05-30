4bb46aae33af27b66c882b8d1d090f2e2bbc105c
Matthieu Pauze <matt.pauze@gmail.com>
2022-05-30 12:04:07 -0400

    Implement Player class initially

--- player.rb
+++ player.rb
@@ -1,4 +1,18 @@
-# Will manage score (lives left) and provide a dead? interface
+# Will manage score (lives left) and provide a dead? validator
 class Player
-  def initialize; end
+  attr_reader :name
+
+  def initialize(name, max_lives = 3)
+    @name = name
+    @max_lives = max_lives
+    @current_lives = max_lives
+  end
+
+  def score
+    "#{@current_lives}/#{@max_lives}"
+  end
+
+  def dead?
+    @current_lives <= 0
+  end
 end
