98790e7ed5f3d8d9fe655a05cd5870b0bc925739
Matthieu Pauze <matt.pauze@gmail.com>
2022-05-30 13:22:48 -0400

    Refactor: Clarify variable naming in Question

--- question.rb
+++ question.rb
@@ -1,11 +1,11 @@
 # Provides a math question along with a validator for the answer
 class Question
-  attr_reader :question
+  attr_reader :text
 
   def initialize(lower_limit = 2, upper_limit = 40)
     @answer = rand(lower_limit..upper_limit)
     difference = rand(1...@answer)
-    @question = "What does #{difference} plus #{@answer - difference} equal?"
+    @text = "What does #{difference} plus #{@answer - difference} equal?"
   end
 
   def answer?(attempt)
