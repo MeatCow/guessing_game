d403312a460a7984006171c484aead170ddbf1a3
Matthieu Pauze <matt.pauze@gmail.com>
2022-05-30 12:22:35 -0400

    Implement turn.rb

--- turn.rb
+++ turn.rb
@@ -1,4 +1,18 @@
+require './question'
 # Will manage the question being asked, as well as who the current player is
 class Turn
-  def initialize; end
+  attr_reader :question
+
+  def initialize(player1, player2)
+    @players = []
+    @players << player1
+    @players << player2
+    @current_player = player1
+    @question = Question.new
+  end
+
+  def next
+    @current_player = @players.rotate![0]
+    @question = Question.new
+  end
 end
